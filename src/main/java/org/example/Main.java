fa73840e6a19f87f57abc6ea8eb3354632034bb2
MerQury14092 <petrakov14092@gmail.com>
2023-05-19 23:56:35 +0300

    Добавление комментариев, удаление ненужного кода

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,97 +5,114 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
-import java.awt.event.WindowStateListener;
 import java.awt.image.BufferedImage;
 import java.net.URL;
 import java.util.stream.IntStream;
 
+
 public class Main {
     public static void main(String[] args) throws Exception{
-        BufferedImage background = ImageIO.read(new URL("https://pngicon.ru/file/uploads/naruto.png")),
-                icon = ImageIO.read(new URL("https://cdn-icons-png.flaticon.com/512/10008/10008838.png")),
-                frTextArea = ImageIO.read(new URL("https://avatanplus.com/files/resources/mid/5777957e9982c155ab1ff6a5.png"));
+        // Подгрузка нужных картинок
+        BufferedImage backgroundImage = ImageIO.read(new URL("https://pngicon.ru/file/uploads/naruto.png")),
+                applicationIcon = ImageIO.read(new URL("https://cdn-icons-png.flaticon.com/512/10008/10008838.png")),
+                textAreaFrame = ImageIO.read(new URL("https://avatanplus.com/files/resources/mid/5777957e9982c155ab1ff6a5.png"));
 
-        ImtResults results = new ImtResults();
+        // Заблаговременная инициализация окна вывода результата
+        ImtResults resultWindow = new ImtResults();
 
+        // Основное окно
         JFrame frame = new JFrame("Калькулятор ИМТ");
-        frame.setIconImage(icon.getScaledInstance(32,32,Image.SCALE_SMOOTH));
+        frame.setIconImage(applicationIcon.getScaledInstance(32,32,Image.SCALE_SMOOTH));
 
+        // Объявление и инициализация компоновщика Grid для нашего окна, а так же установка количества (20x20) ячеек по 20 пикселей
         GridBagLayout layout = new GridBagLayout();
         layout.rowHeights = IntStream.generate(() -> 20).limit(20).toArray();
         layout.columnWidths = IntStream.generate(() -> 20).limit(20).toArray();
         frame.setLayout(layout);
 
-        GridBagConstraints constraints = new GridBagConstraints();
+        // Объект, который будет помогать ставить новые компоненты на нужные места по сетке
+        GridBagConstraints locator = new GridBagConstraints();
 
-        JLabel bckgrnd = new JLabel();
-        bckgrnd.setIcon(new ImageIcon(background.getScaledInstance(400,400,Image.SCALE_SMOOTH)));
+        // Объявление и инициализация фонового изображения
+        JLabel background = new JLabel();
+        background.setIcon(new ImageIcon(backgroundImage.getScaledInstance(400,400,Image.SCALE_SMOOTH)));
 
-        constraints.gridx = 8;
-        constraints.gridy = 10;
-        constraints.gridwidth = 3;
-        constraints.gridheight = 1;
+        // Объявление и инициализация текстового поля ввода веса а так же подгон локатора под нужные координаты
+        locator.gridx = 8;
+        locator.gridy = 10;
+        locator.gridwidth = 3;
+        locator.gridheight = 1;
 
         JTextArea mass = new JTextArea("      ");
-        JLabel mass_bckgrnd = new JLabel();
-        mass_bckgrnd.setIcon(new ImageIcon(frTextArea.getScaledInstance(60,20,Image.SCALE_SMOOTH)));
-        frame.add(mass, constraints);
-        frame.add(mass_bckgrnd, constraints);
+        JLabel mass_background = new JLabel();
+        mass_background.setIcon(new ImageIcon(textAreaFrame.getScaledInstance(60,20,Image.SCALE_SMOOTH)));
+        frame.add(mass, locator);
+        frame.add(mass_background, locator);
 
-        constraints.gridx = 8;
-        constraints.gridy = 12;
-        constraints.gridwidth = 3;
-        constraints.gridheight = 1;
+        // О. и И. текстового поля ввода роста
+        locator.gridx = 8;
+        locator.gridy = 12;
+        locator.gridwidth = 3;
+        locator.gridheight = 1;
 
         JTextArea height = new JTextArea("      ");
-        JLabel height_bckgrnd = new JLabel();
-        height_bckgrnd.setIcon(new ImageIcon(frTextArea.getScaledInstance(60,20,Image.SCALE_SMOOTH)));
-        frame.add(height, constraints);
-        frame.add(height_bckgrnd, constraints);
+        JLabel height_background = new JLabel();
+        height_background.setIcon(new ImageIcon(textAreaFrame.getScaledInstance(60,20,Image.SCALE_SMOOTH)));
+        frame.add(height, locator);
+        frame.add(height_background, locator);
 
-        constraints.gridx = 8;
-        constraints.gridy = 9;
-        constraints.gridwidth = 3;
-        constraints.gridheight = 1;
+
+        // О. и И. подсказки к полю ввода mass
+        locator.gridx = 8;
+        locator.gridy = 9;
+        locator.gridwidth = 3;
+        locator.gridheight = 1;
         JLabel mass_text = new JLabel("Введите массу");
         mass_text.setForeground(Color.WHITE);
-        frame.add(mass_text, constraints);
+        frame.add(mass_text, locator);
 
-        constraints.gridx = 8;
-        constraints.gridy = 11;
-        constraints.gridwidth = 3;
-        constraints.gridheight = 1;
+        // О. и И. подсказки к полю ввода height
+        locator.gridx = 8;
+        locator.gridy = 11;
+        locator.gridwidth = 3;
+        locator.gridheight = 1;
         JLabel height_text = new JLabel("Введите рост");
         height_text.setForeground(Color.WHITE);
-        frame.add(height_text, constraints);
+        frame.add(height_text, locator);
 
-        constraints.gridx = 8;
-        constraints.gridy = 15;
-        constraints.gridwidth = 3;
-        constraints.gridheight = 1;
+        // // О. и И. кнопки расчитать
+        locator.gridx = 8;
+        locator.gridy = 15;
+        locator.gridwidth = 3;
+        locator.gridheight = 1;
         JButton calculate = new JButton("Расчитать");
         calculate.setForeground(Color.BLUE);
-        frame.add(calculate, constraints);
+        frame.add(calculate, locator);
 
-        constraints.gridx = 0;
-        constraints.gridy = 0;
-        constraints.gridwidth = 20;
-        constraints.gridheight = 20;
-        frame.add(bckgrnd, constraints);
+        // Установка фонового изображения
+        locator.gridx = 0;
+        locator.gridy = 0;
+        locator.gridwidth = 20;
+        locator.gridheight = 20;
+        frame.add(background, locator);
 
+        // Стандартные установки настроек для JFrame
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.pack();
         frame.setLocationRelativeTo(null);
 
+        // Добавление функции, которая при нажатии на calculate вызывает метод show у объекта resultWindow и передает туда расчитанные данные
         calculate.addActionListener((e) -> {
             if(isDigit(mass.getText()) && isDigit(height.getText()))
-                results.show(Double.parseDouble(mass.getText())/(Math.pow(Double.parseDouble(height.getText())/100,2)));
+                resultWindow.show(Double.parseDouble(mass.getText())/(Math.pow(Double.parseDouble(height.getText())/100,2)));
         });
 
+        // Показываем окно
         frame.setVisible(true);
 
     }
 
+    // Стандартный предикат проверки строки на число
     private static boolean isDigit(String str){
         try {
             Double.parseDouble(str);
@@ -106,47 +123,33 @@ public class Main {
     }
 
     private static class ImtResults extends JFrame{
+        // Нужные ресурсы, cur_result является temporary решением, остальные подгружаются один раз
         BufferedImage cur_result, skinny, fatty, normal;
-        private long current_time;
 
         public ImtResults() throws Exception {
-            initImages();
+            // Подгрузка ресурсов
+            loadAssets();
 
+            // Стандартные операции над JFrame
             setUndecorated(true);
             getContentPane().setPreferredSize(new Dimension(200,200));
             pack();
             setLocationRelativeTo(null);
+
+            // Слушатель, который при деактивации окна, вызывает close()
             WindowListener listener = new WindowListener() {
                 @Override
-                public void windowOpened(WindowEvent e) {
-
-                }
-
+                public void windowOpened(WindowEvent e) {}
                 @Override
-                public void windowClosing(WindowEvent e) {
-
-                }
-
+                public void windowClosing(WindowEvent e) {}
                 @Override
-                public void windowClosed(WindowEvent e) {
-
-                }
-
+                public void windowClosed(WindowEvent e) {}
                 @Override
-                public void windowIconified(WindowEvent e) {
-
-                }
-
+                public void windowIconified(WindowEvent e) {}
                 @Override
-                public void windowDeiconified(WindowEvent e) {
-
-                }
-
+                public void windowDeiconified(WindowEvent e) {}
                 @Override
-                public void windowActivated(WindowEvent e) {
-
-                }
-
+                public void windowActivated(WindowEvent e) {}
                 @Override
                 public void windowDeactivated(WindowEvent e) {
                     close();
@@ -166,17 +169,19 @@ public class Main {
         }
 
         private void close(){
+            // Закрывает окно
             setVisible(false);
         }
 
         @Override
         public void paint(Graphics g) {
+            // Отрисовывает картинку
             g = getContentPane().getGraphics();
             g.drawImage(cur_result,0,0,200,200,null);
         }
 
-        private void initImages() throws Exception{
-            current_time = 0;
+        private void loadAssets() throws Exception{
+            // Подгружает картинки с заранее подготовленных URL
             cur_result = new BufferedImage(200,200,BufferedImage.TYPE_INT_RGB);
             fatty = ImageIO.read(new URL("https://avatars.mds.yandex.net/i?id=43f4ca0682a7cfe6dfeb6e2cd347d46ea9010b7d-7758455-images-thumbs&n=13"));
             skinny = ImageIO.read(new URL("https://memesmix.net/media/created/kem7sh.jpg"));
